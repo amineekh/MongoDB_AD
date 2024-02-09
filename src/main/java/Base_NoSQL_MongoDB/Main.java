5b9f827f193385d2ef8485342ba4570fee8cfe8f
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2024-02-09 10:32:02 +0100

    09 - 02 -2024
    3 metodos del profe hechos
    
    busqueda_registros() hecho

--- src/main/java/Base_NoSQL_MongoDB/Main.java
+++ src/main/java/Base_NoSQL_MongoDB/Main.java
@@ -7,6 +7,7 @@ import org.bson.Document;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
@@ -17,6 +18,39 @@ public class Main {
         visualizarColeccion();
         System.out.println("voy pasar los datos de un fichero");
         MongoDBToTextFile();
+        System.out.println("voy a buscar un Registro");
+        busqueda_registros();
+
+    }
+
+    private static void busqueda_registros() {
+        Scanner sc = new Scanner(System.in);
+        try (MongoClient mongoClient =  MongoClients.create("mongodb://localhost:27017")){
+            System.out.println("Conexión exitosa a MongoDB.");
+
+            String introducir_nombre_BD="";
+            String introducir_nombre_coleccion="";
+
+            System.out.println("¿introduce el nombre de la base de datos?");
+            introducir_nombre_BD= sc.next();
+            System.out.println("¿ el nombre de la Coleccion?");
+            introducir_nombre_coleccion= sc.next();
+
+
+            MongoDatabase database = mongoClient.getDatabase(introducir_nombre_BD);
+            MongoCollection<Document> collection = database.getCollection(introducir_nombre_coleccion);
+
+            System.out.println("Documentos en la coleccion");
+
+            FindIterable<Document> documents = collection.find();
+
+            for (Document document: documents) {
+                System.out.println(document.toJson());
+            }
+
+        }catch (Exception e){
+            System.err.println("Error al conectar a mongoDB: " + e.getMessage());
+        }
 
     }
 
